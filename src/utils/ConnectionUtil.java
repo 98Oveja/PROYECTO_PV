2ae46a6712be2d6e472c1a58291a327ec63e414a
RonaldoSic <55297894+RonaldoSic@users.noreply.github.com>
2019-11-27 19:58:43 -0600

    New Connection and Data Base

--- src/utils/ConnectionUtil.java
+++ src/utils/ConnectionUtil.java
@@ -9,14 +9,22 @@ public class ConnectionUtil {
 //    Connection conn = null;
     public static Connection conDB()
     {
+        String User ="Ronaldo";
+        String Port =":3306";
+        String Host ="127.0.0.1";
+        String Password ="Datos18";
+        String Database = "PuntoDeVenta";
+        Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-           Connection con = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10312746", "sql10312746", "AA3qZAZqgN");
-            //Connection con = DriverManager.getConnection("jdbc:mysql://localhost/punto_de_venta", "admin_x", "123a");
+            con = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10312746", "sql10312746", "AA3qZAZqgN");
+//             con = DriverManager.getConnection("jdbc:mysql://"+Host+"/"+Database, User, Password);
+
            return con;
 
         } catch (ClassNotFoundException | SQLException ex) {
-            System.err.println("ConnectionUtil : "+ex.getMessage());
+//            System.out.println(con.toString());
+            System.err.println("Data Base isn't connected : "+ex.getMessage());
            return null;
         }
     }
