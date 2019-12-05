e4d2776cc690fceabc0ff98bf521117b88556073
wilian Ed <wilian122397@gmail.com>
2019-12-05 17:08:12 -0600

    minibottons :)

--- src/controllers/Productos/Controller.java
+++ src/controllers/Productos/Controller.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -18,19 +17,34 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable
-{
+public class Controller implements Initializable {
 
 
+    @FXML
+    private Button mini2;
 
+    @FXML
+    private Button mini3;
+
+    @FXML
+    private Button mini4;
+
+    @FXML
+    private Button mini5;
+
+    @FXML
+    private Button mini6;
+    @FXML
+    private Button mini1;
     @FXML
     private ImageView imageview;
     @FXML
     private Button agregarV;
 
-    Connection conexion = null;
-    ConnectionUtil conn = new ConnectionUtil();
+    //   Connection conexion = null;
+    //  ConnectionUtil conn = new ConnectionUtil();
     String direccion;
+
     @FXML
     public void Abrir(ActionEvent actionEvent) throws IOException {
         final Stage primaryStage = new Stage();
@@ -50,24 +64,23 @@ public class Controller implements Initializable
         dialog.show();
     }
 
-    public  String consulta(){
-        String sql = null;
-        sql = "SELECT IMG FROM PRODUCTOS";
-        try {
-            conexion= conn.getConnection();
-            PreparedStatement preparedStatement = conexion.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (!resultSet.next()) {
-                System.out.println("NO FUNCIONA");
-            } else {
-                    direccion = resultSet.getString("IMG");
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return direccion;
-    }
-
+//    public  String consulta(){
+//        String sql = null;
+//        sql = "SELECT IMG FROM PRODUCTOS";
+//        try {
+//            conexion= conn.getConnection();
+//            PreparedStatement preparedStatement = conexion.prepareStatement(sql);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            if (!resultSet.next()) {
+//                System.out.println("NO FUNCIONA");
+//            } else {
+//                    direccion = resultSet.getString("IMG");
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return direccion;
+//    }
 
 
     @Override
@@ -78,5 +91,36 @@ public class Controller implements Initializable
 //        Image image;
 //        image = new Image("file:/"+nueva);
 //        imageview.setImage(image);
+    }
+
+    public void cambio(ActionEvent actionEvent) {
+        if (actionEvent.getSource()==mini2){
+            mini2.setStyle("-fx-background-color: #3B86FF;" +
+                    "-fx-text-fill: #fff;");
+            mini3.setStyle(".PanelLateralOpciones");
+            mini4.setStyle(".PanelLateralOpciones");
+            mini5.setStyle(".PanelLateralOpciones");
+        }
+        else if (actionEvent.getSource()==mini3){
+            mini3.setStyle("-fx-background-color: #3B86FF;" +
+                    "-fx-text-fill: #fff;");
+            mini2.setStyle(".PanelLateralOpciones");
+            mini4.setStyle(".PanelLateralOpciones");
+            mini5.setStyle(".PanelLateralOpciones");
+        }
+        else if (actionEvent.getSource()==mini4){
+            mini4.setStyle("-fx-background-color: #3B86FF;" +
+                    "-fx-text-fill: #fff;");
+            mini5.setStyle(".PanelLateralOpciones");
+            mini2.setStyle(".PanelLateralOpciones");
+            mini3.setStyle(".PanelLateralOpciones");
+        }
+        else if (actionEvent.getSource()==mini5){
+            mini5.setStyle("-fx-background-color: #3B86FF;" +
+                    "-fx-text-fill: #fff;");
+            mini2.setStyle(".PanelLateralOpciones");
+            mini3.setStyle(".PanelLateralOpciones");
+            mini4.setStyle(".PanelLateralOpciones");
+        }
     }
 }
