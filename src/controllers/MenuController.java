fa3503f158450e1f5ad20109da3fd3cd698041b4
laptop_julian <juliorob7@gmail.com>
2019-12-06 14:28:02 -0600

    finalizacion de Animacion Local producto

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -6,6 +6,7 @@ import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
@@ -18,20 +19,31 @@ import java.util.ResourceBundle;
 public class MenuController implements Initializable {
     @FXML
     private ImageView NewImageProducts;
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    private Label titleProduct, brandProduct, desProduct;
+    public void procutosAnimation(){
         Image images = new Image("images/elClubo.jpg");
         Image images1 = new Image("images/ElCluboP.jpg");
         Image images2 = new Image("images/slide_thumb_1.png");
         Image images3 = new Image("images/post_4.png");
 
         Timeline timeline=new Timeline();
-        KeyFrame keyFrame=new KeyFrame(Duration.seconds(1), (event)->{NewImageProducts.setImage(images);});
-        KeyFrame keyFrame2=new KeyFrame(Duration.seconds(2), (event)->{NewImageProducts.setImage(images1);});
-        KeyFrame keyFrame3=new KeyFrame(Duration.seconds(3), (event)->{NewImageProducts.setImage(images2);});
-        KeyFrame keyFrame4=new KeyFrame(Duration.seconds(4), (event)->{NewImageProducts.setImage(images3);});
+        KeyFrame keyFrame=new KeyFrame(Duration.seconds(0), (event)->{NewImageProducts.setImage(images);
+            titleProduct.setText("EL CLUBO");
+            brandProduct.setText("Banda");
+            desProduct.setText("Banda de Rock Nacional Guatemala sdasoijdsa lala lalal lalal lalal");});
+        KeyFrame keyFrame2=new KeyFrame(Duration.seconds(3), (event)->{NewImageProducts.setImage(images1);
+            titleProduct.setText("EL Clubo png");
+            brandProduct.setText("Banda Chapina");
+            desProduct.setText("Banda de Rock Nacional Guatemala fundada en el anio 1996");});
+        KeyFrame keyFrame3=new KeyFrame(Duration.seconds(6), (event)->{NewImageProducts.setImage(images2);
+            titleProduct.setText("Slide_thumb");
+            brandProduct.setText("Fotografia");
+            desProduct.setText("Imagen de transicion prueba sddasjdska asd hasjhd asjdh as");});
+        KeyFrame keyFrame4=new KeyFrame(Duration.seconds(9), (event)->{NewImageProducts.setImage(images3);
+            titleProduct.setText("BasquetBall");
+            brandProduct.setText("Deporte..");
+            desProduct.setText("Aca habra una pequena descripccion de la imagen para poder ver como va todo lo del programa");});
 
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.getKeyFrames().add(keyFrame);
@@ -39,6 +51,11 @@ public class MenuController implements Initializable {
         timeline.getKeyFrames().add(keyFrame3);
         timeline.getKeyFrames().add(keyFrame4);
         timeline.play();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        procutosAnimation();
     }
 
 }
