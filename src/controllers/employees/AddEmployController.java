38615c72fd5c9792de7a7ed9c91fb16301c84dfb
laptop_julian <juliorob7@gmail.com>
2020-01-08 16:05:40 -0600

    Agregando notificaciones

--- src/controllers/employees/AddEmployController.java
+++ src/controllers/employees/AddEmployController.java
@@ -1,19 +1,12 @@
 package controllers.employees;
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTextField;
-import com.sun.mail.handlers.image_gif;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -32,16 +25,14 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import utils.ConnectionUtil;
 import utils.ParseEmail;
-
-import javax.management.Notification;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.sql.*;
-import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.regex.Pattern;
+import javafx.util.Duration;
+
 
 public class AddEmployController implements Initializable {
     public BorderPane imgUser;
@@ -153,7 +144,14 @@ public void CloseModal(){
             if (controller.status == 1) {
                 ClearTextField();//limipiar los textfield
                 querySql(firstName, secondName, firstLastName, secondLastName, direction, numberPhone, placeList.getValue(), email);//metodo insertar
-                Notification
+
+                Notifications.create()
+                        .title("titulo")
+                        .text("texto")
+                        .hideAfter(Duration.seconds(5))
+                        .showError();
+
+
             } else {
                 System.out.println("Cancelo el ingreso");
             }
